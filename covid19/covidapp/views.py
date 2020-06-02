cbf35b28dd0302cc2eb359da60a8c75d1446fa51
Sanay Shah <sanayshah2@gmail.com>
2020-06-03 04:24:50 +0530

    commit

--- covid19/covidapp/views.py
+++ covid19/covidapp/views.py
@@ -33,7 +33,7 @@ def api(request):
     for key,value in all_country.items():
         country_data.append(value)
     country_data.remove('ok')
-     
+
     country_data = sorted(country_data, key=itemgetter('total_cases'),reverse=True)
     
     #response = requests.get('https://api.covid19api.com/summary')
