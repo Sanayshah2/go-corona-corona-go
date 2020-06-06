0207db18a3be8354f91b4d551678798d42bcc840
Sanay Shah <sanayshah2@gmail.com>
2020-06-06 20:00:48 +0530

    debug false

--- covid19/covid19/settings.py
+++ covid19/covid19/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7t=465@ur+x(99i8d)ju@qq9!(%%y5dgb*jx6lyf=it+38=!k*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
